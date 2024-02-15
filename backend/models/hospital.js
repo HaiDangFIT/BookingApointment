0a76225d0303ca8a9f78a8253ade9566b62fe198
HaiDangFIT <dangtqh.fit@gmail.com>
2024-02-15 11:28:26 +0700

    Hospital Model

---
+++ backend/models/hospital.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+
+const hospitalSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    address: {
+        province: {
+            type: String,
+            required: true
+        },
+        district: {
+            type: String,
+            required: true
+        },
+        ward: {
+            type: String,
+            required: true
+        },
+        detail: {
+            type: String,
+            required: true
+        },
+    },
+    specialtyID: [{
+        type: mongoose.Types.ObjectId,
+        ref: "Specialty"
+    }],
+
+    description: {
+        type: String,
+    },
+    image: {
+        type: String,
+    },
+    hostID: {
+        type: mongoose.Types.ObjectId,
+        ref: "User"
+    },
+    rating: [
+        {
+            star: { type: Number },
+            postedBy: { type: mongoose.Types.ObjectId, ref: "User" },
+            comment: { type: String },
+            updatedAt: { type: Date, default: Date.now() },
+        }
+    ],
+    totalRatings: {
+        type: Number,
+        default: 0,
+    },
+})
+
+//Export the model
+module.exports = mongoose.model("Hospital", hospitalSchema);
