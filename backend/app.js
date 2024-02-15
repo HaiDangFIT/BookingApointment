44f494e903dc4cfae1898484135cb5d9a0137e02
HaiDangFIT <dangtqh.fit@gmail.com>
2024-02-15 11:15:57 +0700

    Set up

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const dbConnect = require('./config/dbconnect');
 require("dotenv").config();
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const initRoutes = require("./routes");
 
 const app = express();
 const port = process.env.PORT || 8888;
@@ -8,9 +11,25 @@ const port = process.env.PORT || 8888;
 //connect database
 dbConnect();
 
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ['*'],
+  })
+);
+
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(express.json({ length: 52428800 }));
+
 app.get("/", (req, res) => {
   res.send("Server is running...");
 });
+
+//routes
+initRoutes(app);
 
 app.listen(port, () => {
   console.log("Server running on the port: " + port);
