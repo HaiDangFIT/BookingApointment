0ca8ab5ed6f363702d1da6c0851b8f0f69f8e095
HaiDangFIT <dangtqh.fit@gmail.com>
2024-02-15 11:19:44 +0700

    Login

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,11 @@
 const User = require("../models/user")
 const asyncHandler = require('express-async-handler');
+const {
+    generateAccessToken,
+    generateRefreshToken,
+} = require("../middlewares/jwt");
+const jwt = require("jsonwebtoken");
+const bcryptjs = require("bcrypt");
 
 const register = asyncHandler(async (req, res) => {
     const { firstName, lastName, password, phone, gender } = req.body;
@@ -22,6 +28,43 @@ const register = asyncHandler(async (req, res) => {
     }
 });
 
+const login = asyncHandler(async (req, res) => {
+    const { phone, password } = req.body;
+    if (!phone || !password) {
+        return res.status(400).json({
+            success: false,
+            message: "Vui lòng nhập đầy đủ",
+        });
+    }
+    const response = await User.findOne({ phone });
+    if (response && (await response.isCorrectPassword(password))) {
+        const { password, isBlocked, refreshToken, ...userData } = response.toObject();
+        const accessToken = generateAccessToken(response._id, response.role);
+        // Create refreshToken
+        const newRefreshToken = generateRefreshToken(response._id);
+        // Save refreshToken to database
+        await User.findByIdAndUpdate(
+            response._id,
+            { refreshToken: newRefreshToken },
+            { new: true }
+        );
+        // Save refreshToken to cookie
+        res.cookie("refreshToken", newRefreshToken, {
+            httpOnly: true,
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+        });
+        res.setHeader("Authorization", `Bearer ${accessToken}`);
+        return res.status(200).json({
+            success: true,
+            accessToken,
+            data: userData,
+        });
+    } else {
+        throw new Error("Tài khoản hoặc mật khẩu không trùng khớp!");
+    }
+});
+
 module.exports = {
     register,
+    login,
 }
